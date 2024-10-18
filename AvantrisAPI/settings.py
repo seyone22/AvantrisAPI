ea529dd0382efa2c3b594c26f0bfcabc505afd9c
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-10-18 10:03:19 +0530

    Added JWT auth

--- AvantrisAPI/settings.py
+++ AvantrisAPI/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -98,6 +100,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Added JWT auth
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 
 # Internationalization
--- AvantrisAPI/views.py
+++ AvantrisAPI/views.py
@@ -1,8 +1,12 @@
 # views.py
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import User
 from .serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
