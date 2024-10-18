d5e4e181ba57f6c8c1bb18bce8ab821e632b52b4
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-10-18 09:34:05 +0530

    Created models for user and item

---
+++ AvantrisAPI/models.py
@@ -0,0 +1,15 @@
+# models.py
+from django.db import models
+
+class Item(models.Model):
+    item_id = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+
+
+class User(models.Model):
+    user_id = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    items = models.ManyToManyField(Item)
+
