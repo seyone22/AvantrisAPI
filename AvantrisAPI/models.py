a6db52519b72a0821ab856f6a3081b305902b8af
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-10-18 09:58:44 +0530

    Added comments

--- AvantrisAPI/models.py
+++ AvantrisAPI/models.py
@@ -1,11 +1,13 @@
 # models.py
 from django.db import models
 
+# Model for Item
 class Item(models.Model):
     item_id = models.CharField(max_length=255)
     quantity = models.IntegerField()
     price = models.FloatField()
 
+# Model for User, many to many relation to items.
 class User(models.Model):
     user_id = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
